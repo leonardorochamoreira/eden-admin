4ec1ff25b752ab696b000df485d33c6c0c31690b
Leonardo <leonardorochamoreira.rj@gmail.com>
2016-06-28 17:38:28 -0300

    commit inicial

---
+++ www/js/app.js
@@ -0,0 +1,208 @@
+var app = angular.module('app_eden', ['ionic', 'app_eden.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+
+    .state('app.novo_evento', {
+      url: '/novo_evento',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/novo_evento.html',
+          controller: 'NovoEventoCtrl'
+        }
+      }
+    })
+
+    .state('app.novo_usuario', {
+      url: '/novo_usuario',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/novo_usuario.html',
+          controller: 'NovoUsuarioCtrl'
+        }
+      }
+    })
+
+
+    .state('app.eventos', {
+      url: '/eventos',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/eventos.html',
+          controller: 'EventosCtrl'
+        }
+      }
+    })
+
+    .state('app.usuarios_eventos', {
+      url: '/usuarios_eventos/:evento_id',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/usuarios_eventos.html',
+          controller: 'UsuariosEventosCtrl'
+        }
+      }
+    })
+
+
+    .state('app.atualizar_evento', {
+      url: '/atualizar_evento/:evento_id',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/atualizar_evento.html',
+          controller: 'AtualizarEventoCtrl'
+        }
+      }
+    })
+
+  .state('app.detalhes_evento', {
+    url: '/detalhes_evento/:evento_id',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/detalhes_evento.html',
+        controller: 'DetalheEventoCtrl'
+      }
+    }
+  });
+  $urlRouterProvider.otherwise('/app/eventos');
+});
+
+
+app.directive('fileModel', ['$parse', function ($parse) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var model = $parse(attrs.fileModel);
+      var modelSetter = model.assign;
+
+      element.bind('change', function(){
+        scope.$apply(function(){
+          var file = (element[0].files[0]);
+          scope.file_selected = file.name;
+          modelSetter(scope, element[0].files[0]);
+        });
+      });
+    }
+  };
+}]);
+
+app.service('EventoService', ['$http', function ($http,$scope) {
+  this.add = function(Evento,url_upload,$state){
+    var fd = new FormData();
+    fd.append('file', Evento.img);
+    fd.append('titulo', Evento.titulo);
+    fd.append('sub_titulo', Evento.sub_titulo);
+    fd.append('descricao', Evento.descricao);
+    fd.append('data_evento', Evento.data_evento);
+
+    $http.post(url_upload, fd, {
+      transformRequest: angular.identity,
+      headers: {'Content-Type': undefined}
+    })
+      .success(function(){
+
+        alert("Adicionado com sucesso!");
+        $state.go("app.eventos",{reload: true});
+        $scope.nao_exibir_loading = true;
+        //window.location.assign("http://localhost/eden/www/#/app/eventos")
+      })
+      .error(function(){
+      });
+  };
+
+  this.update = function(Evento,url_upload,$state){
+    var fd = new FormData();
+    fd.append('id', Evento.id);
+    fd.append('file', Evento.img);
+    fd.append('titulo', Evento.titulo);
+    fd.append('sub_titulo', Evento.sub_titulo);
+    fd.append('descricao', Evento.descricao);
+    fd.append('data_evento', Evento.data_evento);
+
+    $http.post(url_upload, fd, {
+      transformRequest: angular.identity,
+      headers: {'Content-Type': undefined}
+    })
+      .success(function(){
+        alert("Atualizado com sucesso!");
+        $state.go("app.eventos",{reload: true});
+        $scope.nao_exibir_loading = true;
+        //window.location.assign("http://localhost/eden/www/#/app/eventos")
+      })
+      .error(function(){
+      });
+  }
+}]);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+app.service('UsuarioService', ['$http', function ($http) {
+  this.add = function(Usuario,url_upload,$state,$scope){
+    var fd = new FormData();
+    fd.append('id', Usuario.id);
+    fd.append('file', Usuario.img);
+    fd.append('nome', Usuario.nome);
+    fd.append('email', Usuario.email);
+
+    $http.post(url_upload, fd, {
+      transformRequest: angular.identity,
+      headers: {'Content-Type': undefined}
+    })
+      .success(function(){
+
+        alert("Adicionado com sucesso!");
+        $state.go("app.usuarios_eventos",{reload: true});
+        $scope.nao_exibir_loading = true;
+        //window.location.assign("http://localhost/eden/www/#/app/usuarios_eventos")
+      })
+      .error(function(){
+      });
+  };
+
+  this.update = function(Evento,url_upload,$state){
+    var fd = new FormData();
+    fd.append('id', Usuario.id);
+    fd.append('file', Usuario.img);
+    fd.append('nome', Usuario.nome);
+    fd.append('email', Usuario.email);
+
+    $http.post(url_upload, fd, {
+      transformRequest: angular.identity,
+      headers: {'Content-Type': undefined}
+    })
+      .success(function(){
+        alert("Atualizado com sucesso!");
+        $state.go("app.usuarios_eventos",{reload: true});
+        $scope.nao_exibir_loading = true;
+        //window.location.assign("http://localhost/eden/www/#/app/usuarios_eventos")
+      })
+      .error(function(){
+      });
+  }
+}]);
---
+++ www/js/controllers.js
@@ -0,0 +1,204 @@
+angular.module('app_eden.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  ///////////spinner
+  ///////////$scope.nao_exibir_loading = true;
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+
+
+  .controller('NovoEventoCtrl', function($scope,$http,$state,EventoService) {
+
+    $scope.data_hoje = function(){
+      var data = new Date();
+      var dia = data.getDate();
+      var mes = data.getMonth() + 1;
+      var ano = data.getFullYear();
+      return [dia, mes, ano].join('/');
+
+    };
+
+    $scope.nao_exibir_loading = true;
+
+    $scope.adicionar_evento = function(Evento){
+
+      $scope.nao_exibir_loading = false;
+
+      // console.log('Evento contro');
+      // console.log(Evento);
+
+
+      var url_upload = "http://api-eden.cursophprj.com.br/eventos/insert";
+      //var url_upload = "http://localhost/eden/api/eventos/insert";
+      EventoService.add(Evento,url_upload,$state);
+
+
+    };
+  })
+
+
+  .controller('NovoUsuarioCtrl', function($scope,$http,$state,UsuarioService) {
+
+    $scope.nao_exibir_loading = true;
+
+    $scope.adicionar_usuario = function(Usuario){
+
+    $scope.nao_exibir_loading = false;
+
+      var url_upload = "http://api-eden.cursophprj.com.br/usuario/insert";
+      //var url_upload = "http://localhost/eden/api/eventos/insert";
+      UsuarioService.add(Usuario,url_upload,$state);
+
+
+      //var url_upload = "http://api-eden.cursophprj.com.br/eventos/list";
+      //var url_upload = "http://localhost/eden/api/eventos/insert";
+      //UsuarioService.add(Usuario,url_upload,$state,$scope);
+
+      };
+  })
+
+
+  .controller('EventosCtrl', function($scope,$http) {
+
+    $http.get("http://api-eden.cursophprj.com.br/eventos") .then(function(response) {
+      $scope.eventos = (response.data.eventos);
+
+    });
+
+    $scope.excluir_evento = function (id) {
+      $http.post('http://api-eden.cursophprj.com.br/eventos/delete', {'id':id});
+      alert("Excluido com sucesso!");
+    };
+
+    $scope.listarTodosEventos = function () {
+      $http.get("http://api-eden.cursophprj.com.br/eventos") .then(function(response) {
+        $scope.eventos = (response.data.eventos);
+      });
+    };
+
+    $scope.excluir_evento = function (id) {
+      $http.post('http://api-eden.cursophprj.com.br/eventos/delete', {'id':id})
+        .success(function (){
+            $scope.listarTodosEventos();
+          }
+        );
+    }
+  })
+
+
+  .controller('UsuariosEventosCtrl', function($scope,$http) {
+
+    $scope.nao_exibir_loading=false;
+
+    $http.get("http://api-eden.cursophprj.com.br/usuario/list") .then(function(response) {
+
+      $scope.quantidade_usuarios = (response.data.rowCount);
+
+      $scope.lista_usuarios = (response.data.data_usuarios);
+    });
+
+    $http.get("http://api-eden.cursophprj.com.br/usuario/list") .then(function(response) {
+      $scope.eventos = (response.data.data_usuarios);
+
+    });
+
+    $scope.excluir_evento = function (id) {
+      $http.post('http://api-eden.cursophprj.com.br/eventos/delete', {'id':id});
+      alert("Excluido com sucesso!");
+    };
+
+
+    $scope.listarTodosUsuarios = function () {
+      $http.get(" http://api-eden.cursophprj.com.br/usuario/list") .then(function(response) {
+        $scope.lista_usuarios = (response.data.data_usuarios);
+      });
+    };
+
+    $scope.excluir_evento = function (id) {
+      $http.post('http://api-eden.cursophprj.com.br/eventos/delete', {'id':id})
+        .success(function (){
+            $scope.listarTodosUsuarios();
+          }
+        );
+    }
+
+  })
+
+
+  .controller('AtualizarEventoCtrl', function($scope,$stateParams,$http,EventoService,$state) {
+
+    $scope.nao_exibir_loading = true;
+
+    var evento_id = $stateParams.evento_id;
+
+    $scope.evento_id = evento_id;
+
+    $http.get("http://api-eden.cursophprj.com.br/eventos/"+evento_id) .then(function(response) {
+            $scope.Evento = (response.data.eventos);
+
+    });
+
+
+    $scope.excluir_evento = function (id) {
+      $http.post('http://api-eden.cursophprj.com.br/eventos/delete', {'id':id})
+        .success(function (){
+            $scope.listarTodosEventos();
+          }
+        )};
+
+      $scope.atualizar_evento = function (Evento) {
+        $scope.nao_exibir_loading = false;
+      var url_upload = "http://api-eden.cursophprj.com.br/eventos/update";
+      //var url_upload = "http://localhost/eden/api/eventos/update";
+      EventoService.update(Evento,url_upload,$state);
+      //$http.post('http://api-eden.cursophprj.com.br/eventos/update', {'id':Evento.id,'titulo':Evento.titulo,sub_titulo:Evento.sub_titulo,'descricao':Evento.descricao,'data_evento':Evento.data_evento});
+      //alert("Atualizado com sucesso!");
+    }
+  })
+
+
+.controller('DetalheEventoCtrl', function($scope,$stateParams,$http) {
+  $http.get("http://api-eden.cursophprj.com.br/usuario/list") .then(function(response) {
+
+    $scope.quantidade_usuarios = (response.data.rowCount);
+  });
+
+  var evento_id = $stateParams.evento_id;
+  $scope.evento_id = evento_id;
+
+  $http.get("http://api-eden.cursophprj.com.br/eventos/"+evento_id) .then(function(response) {
+    $scope.evento_selecionado = (response.data.eventos);
+
+  })
+
+});
